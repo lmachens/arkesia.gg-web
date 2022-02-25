af09a541c786914fa1dc7a5f6e56dea20a75084f
lmachens <leon@machens.koeln>
2022-02-25 11:38:28 +0100

    💄 Add pointer style

--- app/components/ImagePreview.tsx
+++ app/components/ImagePreview.tsx
@@ -26,6 +26,9 @@ export default function ImagePreview({ src }: ImagePreviewProps) {
         radius="sm"
         onClick={() => setOpened(true)}
         withPlaceholder
+        sx={() => ({
+          cursor: "pointer",
+        })}
       />
     </>
   );
