cff18adbc726d9e21962788aae1bc3471f4851bc
lmachens <leon@machens.koeln>
2022-03-19 11:41:14 +0100

    🔍 Add meta tags

--- app/routes/maps/$continent.$area.tsx
+++ app/routes/maps/$continent.$area.tsx
@@ -4,9 +4,24 @@ import { AppShell, LoadingOverlay } from "@mantine/core";
 import { nodeAction } from "~/lib/actions.server";
 import { areaLoader } from "~/lib/loaders.server";
 import AppBreadcrumbs from "~/components/AppBreadcrumbs";
+import type { MetaFunction } from "remix";
 
 export const loader = areaLoader;
 export const action = nodeAction;
+
+export const meta: MetaFunction = ({ params }) => {
+  if (params.continent === "World") {
+    return {
+      title: "Lost Ark Map - Arkesia.gg",
+      description:
+        "Arkesia is an interactive map with mokoko seeds, hidden stories and more for Lost Ark.",
+    };
+  }
+  return {
+    title: `${params.area} in ${params.continent} - Arkesia.gg`,
+    description: `Interactive map with mokoko seeds, hidden stories and more resources in ${params.continent} / ${params.area} for Lost Ark.`,
+  };
+};
 
 export default function MapPage() {
   return (
