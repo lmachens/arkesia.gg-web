7648ac8b2830066f10fb786568a9c288350c0748
lmachens <leon@machens.koeln>
2022-03-20 11:59:20 +0100

    🔍 Add sitemap.xml

---
+++ app/routes/[sitemap.xml].tsx
@@ -0,0 +1,26 @@
+import type { LoaderFunction } from "remix";
+import { continents } from "~/lib/static";
+
+export const loader: LoaderFunction = async () => {
+  const urls: string[] = [];
+  continents.forEach((continent) => {
+    continent.areas.forEach((area) => {
+      urls.push(
+        `<url><loc>https://arkesia.gg/maps/${continent.name}/${area.name}</loc></url>`
+      );
+    });
+  });
+
+  const content = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join(
+    ""
+  )}</urlset>`;
+
+  return new Response(content, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml",
+      "xml-version": "1.0",
+      encoding: "UTF-8",
+    },
+  });
+};
