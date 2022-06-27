3b9a5d20d416820ef86fbb075af0e15cba53f1f1
lmachens <leon@machens.koeln>
2022-06-27 09:42:32 +0200

    🐛 Replace copy logic for Overwolf browser

--- app/components/ShareButton.tsx
+++ app/components/ShareButton.tsx
@@ -4,6 +4,29 @@ import { Share1Icon } from "@modulz/radix-icons";
 import { areaContinents } from "~/lib/static";
 import type { AreaNodeLocationDTO } from "~/lib/types";
 
+const copyTextToClipboard = (text: string) => {
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    const successful = document.execCommand("copy");
+    return successful ? "successful" : "unsuccessful";
+  } catch (err) {
+    return false;
+  } finally {
+    document.body.removeChild(textArea);
+  }
+};
+
 type ShareButtonProps = {
   areaNodeLocation: AreaNodeLocationDTO;
 };
@@ -21,7 +44,7 @@ const ShareButton = ({ areaNodeLocation }: ShareButtonProps) => {
       await navigator.share(shareData);
     } catch (error) {
       console.error(error);
-      navigator.clipboard.writeText(shareData.url);
+      copyTextToClipboard(shareData.url);
       notifications.showNotification({
         title: "Link copied to clipboard",
         message: "",
